b19c1e59136fbfa93f697f39302a5ecccada3a39
Sebastien LANGE <sebastien.lange@Syleam.fr>
2011-01-30 14:04:47 +0100

    [IMP] crm_timesheet: add help in field

--- object/account.py
+++ object/account.py
@@ -27,7 +27,7 @@ class account_analytic_account(osv.osv):
     _inherit = 'account.analytic.account'
 
     _columns = {
-        'rounding_duration': fields.float('Rouding Duration'),
+        'rounding_duration': fields.float('Rouding Duration', help="This field allow to rounding duration of cases.\nExample :\n- value to 00:15, we have a case with timesheet to 00:35, the duration will be 00:45\n- value to 00:15, we have a case with timesheet to 00:30, the duration will be 00:30"),
     }
 
 account_analytic_account()
